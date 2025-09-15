a9e7cc13d8e48c29fd78861b954034cb674a5c02
Yash <info.yashkumar.09@gmail.com>
2025-09-15 19:33:46 +0530

    Certificate UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,70 @@ Font.register({
 
 const styles = StyleSheet.create({
   page:{
-    padding: 40
+    padding: 40,
+    fontSize: 14,
+    textAlign: 'center',
+    backgroundColor: '#fdfdfd',
+    height: '100%'
+  },
+  border:{
+    border: "5pt solid #1d4ed8",
+    padding: 20,
+    borderRadius: 10,
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  logo:{
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    alignSelf: "center",
+    borderRadius: 60,
+    objectFit: "cover"
+  },
+  title:{
+    fontSize: 30,
+    marginBottom: 20,
+    fontFamily: 'Oswald',
+    color: "#1d4ed8"
+  },
+  subtitle:{
+    fontSize: 18,
+    marginBottom: 40,
+    fontFamily: "Times-Roman",
+    color: "#444"
+  },
+  name:{
+    fontSize: 26,
+    marginVertical: 20,
+    fontFamily: "Oswald",
+    color: "#111"
+  },
+  body:{
+    fontSize: 16,
+    marginBottom: 40,
+    fontFamily: "Times-Roman"
+  },
+  footer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 50,
+    paddingHorizontalL: 40
+  },
+  signature:{
+    borderTop: "1pt solid #000",
+    width: 150,
+    textAlign: "center",
+    paddingTop: 5,
+    fontSize: 12,
+    fontFamily: "Times-Roman"
+  },
+  date:{
+    marginTop: 20,
+    fontSize: 12,
+    color: "grey"
   }
 
 })
--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,10 +1,21 @@
-import { Document, Page, Text } from '@react-pdf/renderer'
+import { Document, Image, Page, Text, View } from '@react-pdf/renderer'
 
 const Certificate = ({styles}) => {
   return (
     <Document>
         <Page style={styles.page}>
-            <Text>Hello React PDF</Text>
+            <View style={styles.border}>
+                <Image style={styles.logo} src="https://images.unsplash.com/photo-1757137910091-1cf071030691?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMxfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"/>
+                <Text style={styles.title}>Certificate of Achievement</Text>
+                <Text style={styles.subtitle}>This Certificate is proudly presented to</Text>
+                <Text style={styles.name}>{name || "Your name here"}</Text>
+                <Text style={styles.body}>For Outstanding performance and dedication. We celebrate your achievement and wish you continued success.</Text>
+                <View style={styles.footer}>
+                    <Text style={styles.signature}>Signature</Text>
+                    <Text style={styles.signature}>Director</Text>
+                </View>
+                <Text style={styles.date}>Date: {new Date().toLocaleDateString()}</Text>
+            </View>
         </Page>
     </Document>
   )
